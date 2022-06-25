bc53380828a08b8f1f53405f2dd273008cf51362
womamire <mihretutd@gmail.com>
2022-06-25 14:19:05 +0300

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+/*
+ * Auth: @womamire
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argv = argc;
+	printf("%d\n", argc + 1);
+	return (0);
+}
