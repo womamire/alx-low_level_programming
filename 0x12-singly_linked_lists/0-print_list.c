4ca5432f04b7aa79ae61e853d95510b5e98126f7
womamire <mihretutd@gmail.com>
2022-07-15 03:04:26 +0300

    C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * print_list - Print all the elements of a list_t list
  * @h: data type pointer of struct
  * Return: elements of the str i
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t i;
 	const list_t *tmp;
-	
+
 	i = 0;
 	tmp = h;
 	while (tmp != NULL)
