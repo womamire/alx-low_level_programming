d23de47c6dd677e7db99bb1d3b5782ad9138ae8f
womamire <mihretutd@gmail.com>
2022-07-25 22:29:53 +0300

    C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef
 #define 
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x15-file_io directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
